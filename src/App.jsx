e77f2bc66505b8c6dff1e5670163808a5f064645
nikhilranjan01 <nikhilranjan2004@gmail.com>
2026-01-17 16:24:43 +0530

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,22 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+      <main className="pt-16">
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
 
-      {/* More sections like About, Projects, etc. */}
+      <Footer />
     </>
   );
 }
